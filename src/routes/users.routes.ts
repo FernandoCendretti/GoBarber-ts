8cdf8bd607f9656158e52e22fb04c79de27bec1b
FernandoCendrettiSolucx <fernando.cendretti@solucx.com.br>
2020-05-20 12:13:11 -0300

    Upload avatar user

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,13 @@
+import multer from 'multer';
 import { Router } from 'express';
 
 import CreateUserService from '../service/CreateUserService';
+import UpdateUserAvatarService from '../service/UpdateUserAvatarService';
+import ensureAutheticated from '../middlewares/ensureAuthenticate';
+import uploadConfig from '../config/upload';
 
 const userRouter = Router();
+const upload = multer(uploadConfig);
 
 userRouter.post('/', async (request, response) => {
     try {
@@ -17,5 +22,27 @@ userRouter.post('/', async (request, response) => {
         return response.status(400).json({ error: err.message });
     }
 });
+
+userRouter.patch(
+    '/avatar',
+    ensureAutheticated,
+    upload.single('avatar'),
+    async (request, response) => {
+        try {
+            const updateUserAvatar = new UpdateUserAvatarService();
+
+            const user = await updateUserAvatar.execute({
+                user_id: request.user.id,
+                avatarFileName: request.file.filename,
+            });
+
+            delete user.password;
+
+            return response.json(user);
+        } catch (err) {
+            return response.status(400).json({ error: err.message });
+        }
+    },
+);
 
 export default userRouter;
